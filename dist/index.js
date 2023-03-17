1d808a10bc42b6e09a2ef85d7307d0a8bcda9a3a
tejusk06 <tejus.kabadi@gmail.com>
2023-03-17 12:38:26 +0530

    logic to add locations 2

--- dist/index.js
+++ dist/index.js
@@ -200,6 +200,15 @@
         }
       });
     });
+    const locations = document.querySelectorAll('[discover-element="location-wrap"]');
+    const locationCategories = document.querySelectorAll('[discover-element="location-category-wrap"]');
+    locationCategories.forEach((eachLocationCategory) => {
+      locations.forEach((eachLocation) => {
+        if (eachLocationCategory.querySelector(".home_filters_checkbox-label").innerHTML === eachLocation.querySelector(".home_filters_location-category").innerHTML) {
+          eachLocationCategory.querySelector(".home_filters_location-wrap").append(eachLocation);
+        }
+      });
+    });
     let scrollPosition;
     setInterval(() => {
       scrollPosition = $(window).scrollTop();
